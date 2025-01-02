24fde3b959a72f83f127e1171da03d96e939f60f
raygeeknyc <raygeeknyc@gmail.com>
2025-01-01 22:15:47 -0500

    remove auth file from tracking

--- talentvoting/webclient/public/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBRcmoJ4bh7oXq2hSS3rvj29ekhvnVTDIM",
-    authDomain: "votingforthestars.firebaseapp.com",
-    projectId: "votingforthestars",
-    storageBucket: "votingforthestars.firebasestorage.app",
-    messagingSenderId: "84591860",
-    appId: "1:84591860:web:4910ae28628d7ee3f14aa1"
-  };
