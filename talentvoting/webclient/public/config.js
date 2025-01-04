3515d0280f4027092391757bf46fc17c5ad56a31
raygeeknyc <raygeeknyc@gmail.com>
2025-01-04 18:35:18 -0500

    Login/logout working

---
+++ talentvoting/webclient/public/config.js
@@ -0,0 +1,5 @@
+export const firebaseAuthIdentifier = {
+  apiKey: "AIzaSyAdTvc9qHyo0HHjyRw66KHFVn5sWPlgXLQ",
+  authDomain: "votingforthestars.firebaseapp.com",
+  projectId: "votingforthestars"
+};
