a7c2991fc065c8d0e3eac69b455c7df477267a84
raygeeknyc <raygeeknyc@gmail.com>
2025-01-03 20:11:02 -0500

    resolve chrome developer console issues in deployed app
    add logo

---
+++ talentvoting/webclient/public/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC7UQ6-49XvHDwlsxtXM43YmwiRfDurUoI",
+    authDomain: "votingforthestars.firebaseapp.com",
+    projectId: "votingforthestars",
+    storageBucket: "votingforthestars.firebasestorage.app",
+    messagingSenderId: "84591860",
+    appId: "1:84591860:web:4910ae28628d7ee3f14aa1"
+  };
