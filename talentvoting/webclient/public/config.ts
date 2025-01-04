bbdf7888e3b4943fe648494e55b7dc36a3f74a5b
raygeeknyc <raygeeknyc@gmail.com>
2025-01-04 15:28:18 -0500

    Remove config from tracking

--- talentvoting/webclient/public/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "XXX"
-  authDomain: "votingforthestars.firebaseapp.com",
-  projectId: "votingforthestars",
-  storageBucket: "votingforthestars.firebasestorage.app",
-  messagingSenderId: "84591860",
-  appId: "1:84591860:web:4910ae28628d7ee3f14aa1"
-};
