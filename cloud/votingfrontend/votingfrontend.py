50ea44c0d81d5d68bd5b92fadcb4dce96a587843
raygeeknyc <raygeeknyc@gmail.com>
2025-01-12 17:59:37 -0500

    Sending token Submitting form from JavaScript Static resources deployed and routed properly

--- cloud/votingfrontend/votingfrontend.py
+++ cloud/votingfrontend/votingfrontend.py
@@ -9,63 +9,67 @@ from firebase_admin import credentials, auth
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
+import sys
+
 cred = credentials.Certificate('private/serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def root():
-    return "<html><body>voting front end service</body></html>"
+     print(str(request.form), file=sys.stderr)
+     return "<html><body>voting front end service</body></html>"
 
 
 @app.route('/api/verify', methods=['POST'])
 def verify_token():
-    id_token = request.form['idToken']
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-        # Get user information from the decoded token
-        uid = decoded_token['uid']
-        # Do something with the user information
-        return jsonify({'success': True, 'uid': uid})
-    except auth.InvalidIdTokenError:
-        return jsonify({'success': False, 'error': 'Invalid ID token'})    
+     print(str(request.form), file=sys.stderr)
+     id_token = request.form['idToken']
+     try:
+         decoded_token = auth.verify_id_token(id_token)
+         # Get user information from the decoded token
+         uid = decoded_token['uid']
+         # Do something with the user information
+         return jsonify({'success': True, 'uid': uid})
+     except auth.InvalidIdTokenError:
+         return jsonify({'success': False, 'error': 'Invalid ID token'})    
 
 class VotingFrontEnd(VoteIngester):
-    @staticmethod
-    def _isLoggedInUser(user) ->bool:
-        if user:
-            return True
-        else:
-            return False
+     @staticmethod
+     def _isLoggedInUser(user) ->bool:
+         if user:
+             return True
+         else:
+             return False
     
-    def __init__(self):
-        super().__init__()
-        self._policy_engine = VotingPolicyEngine()
+     def __init__(self):
+         super().__init__()
+         self._policy_engine = VotingPolicyEngine()
     
-    def _getActs(self) ->Acts:
-        return self._policy_engine.getAllActs()
+     def _getActs(self) ->Acts:
+         return self._policy_engine.getAllActs()
    
-    def getAllActs(self, requesting_user) ->any:
-        if not self._isLoggedInUser(requesting_user):
-            return  InvalidUser(requesting_user).response()
+     def getAllActs(self, requesting_user) ->any:
+         if not self._isLoggedInUser(requesting_user):
+             return  InvalidUser(requesting_user).response()
 
-        return self._getActs()
+         return self._getActs()
     
-    def cast(self, requesting_user, act:Act) ->any:
-        if not self._isLoggedInUser(requesting_user):
-            return  InvalidUser(requesting_user).response()
+     def cast(self, requesting_user, act:Act) ->any:
+         if not self._isLoggedInUser(requesting_user):
+             return  InvalidUser(requesting_user).response()
 
-        if not self._policy_engine.isEligibleVote(requesting_user, act):
-            return IneligibleVote(requesting_user, act).response()
+         if not self._policy_engine.isEligibleVote(requesting_user, act):
+             return IneligibleVote(requesting_user, act).response()
     
-    def getEligibleActs(self, requesting_user) ->Acts:
-        if not self._isLoggedInUser(requesting_user):
-            return  InvalidUser(requesting_user).response()
+     def getEligibleActs(self, requesting_user) ->Acts:
+         if not self._isLoggedInUser(requesting_user):
+             return  InvalidUser(requesting_user).response()
 
-        candidate_acts = self._getActs()
-        eligible_acts = []
-        for act in candidate_acts:
-            if act["voting_eligible"]:
-                eligible_acts.append(act)
+         candidate_acts = self._getActs()
+         eligible_acts = []
+         for act in candidate_acts:
+             if act["voting_eligible"]:
+                 eligible_acts.append(act)
         
-        return eligible_acts 
+         return eligible_acts 
